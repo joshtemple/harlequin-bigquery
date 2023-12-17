222aff77b5ef2cc7af57726af0c56bc5e8c272b2
Josh Temple <8672171+joshtemple@users.noreply.github.com>
2023-12-16 21:06:18 -0500

    Add validators for project and region
    This also helps to protect against SQL injection, since unusual SQL won't be allowed in

--- src/harlequin_bigquery/cli_options.py
+++ src/harlequin_bigquery/cli_options.py
@@ -1,15 +1,34 @@
-from harlequin.options import (
-    FlagOption,  # noqa
-    ListOption,  # noqa
-    PathOption,  # noqa
-    SelectOption,  # noqa
-    TextOption,
-)
+import re
+
+from harlequin.options import TextOption
+
+
+def is_valid_project(project: str) -> tuple[bool, str | None]:
+    is_valid = (
+        re.match(r"^[a-z][a-z0-9-]{4,28}[a-z0-9]$", project, flags=re.IGNORECASE)
+        is not None
+    )
+    return (
+        is_valid,
+        "Must provide a valid project ID" if not is_valid else None,
+    )
+
+
+def is_valid_region(region: str) -> tuple[bool, str | None]:
+    is_valid = (
+        re.match(r"^[a-z][a-z0-9-]+[a-z0-9]$", region, flags=re.IGNORECASE) is not None
+    )
+    return (
+        is_valid,
+        "Must provide a valid region" if not is_valid else None,
+    )
+
 
 project = TextOption(
     name="project",
     description="The project ID to use for the BigQuery connection",
     short_decls=["-p"],
+    validator=is_valid_project,
 )
 
 location = TextOption(
